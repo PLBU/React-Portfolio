4287fb195f0efe9c6e95e6f12365be9ff3f28ebd
PLBU <johanes123renaldi@gmail.com>
2020-07-17 18:32:15 +0700

    Added link for Google Play and Image social tag

--- src/index.js
+++ src/index.js
@@ -14,16 +14,16 @@ render(
       <meta prefix="og: http://ogp.me/ns#" name="description" content="A website about Renaldi Arlin, his projects, and ways to reach him."/>
 
       <meta prefix="og: http://ogp.me/ns#" property="og:type" content="website"/>
-      <meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://cocky-pare-03f8b1.netlify.app/"/>
+      <meta prefix="og: http://ogp.me/ns#" property="og:url" content="https://linar.site/"/>
       <meta prefix="og: http://ogp.me/ns#" property="og:title" content="Renaldi Arlin"/>
       <meta prefix="og: http://ogp.me/ns#" property="og:description" content="A website about Renaldi Arlin, his projects, and ways to reach him."/>
-      <meta prefix="og: http://ogp.me/ns#" property="og:image" content="https://mmc.tirto.id/image/otf/1024x535/2016/07/26/TIRTO-20140522_batman_warner-bros_ratio-16x9.jpg"/>
+      <meta prefix="og: http://ogp.me/ns#" property="og:image" content="https://imgur.com/7CT9GBH"/>
 
       <meta property="twitter:card" content="summary_large_image"/>
-      <meta property="twitter:url" content="https://cocky-pare-03f8b1.netlify.app/"/>
+      <meta property="twitter:url" content="https://linar.site/"/>
       <meta property="twitter:title" content="Renaldi Arlin"/>
       <meta property="twitter:description" content="A website about Renaldi Arlin, his projects, and ways to reach him."/>
-      <meta property="twitter:image" content="https://mmc.tirto.id/image/otf/1024x535/2016/07/26/TIRTO-20140522_batman_warner-bros_ratio-16x9.jpg"/>
+      <meta property="twitter:image" content="https://imgur.com/7CT9GBH"/>
     </Helmet>
     <App/>
   </React.Fragment>,
